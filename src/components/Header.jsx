e90b5f863312e178782eb1d797a77a07829e1cba
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-03-05 16:56:33 +0600

    header issue fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,27 +10,44 @@ const Header = () => {
   };
 
   return (
-    <div className="flex justify-between items-center h-24 px-4 text-white bg-[#00fd9a]">
-      <h1 className="w-full text-3xl font-bold text-white">Book Share App</h1>
+    <div className="flex justify-between items-center px-4 text-white bg-[#6ea76e]">
+      <div className="flex items-center">
+        <img
+          src="/images/bookShare.jpg"
+          className="w-[30px] h-[30px] rounded"
+        />
+        <h1 className="w-full text-3xl font-bold text-white ms-2">
+          Book Share
+        </h1>
+      </div>
+
       <ul className="hidden md:flex text-xl">
         <li className="p-4">Home</li>
         <li className="p-4">Dashboard</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
+
       <div onClick={handleNav} className="block md:hidden">
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
+
       <div
         className={
-          nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-white bg-[#6ea76e] ease-in-out duration-500"
-            : "ease-in-out duration-500 fixed left-[-100%]"
+          `${nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-1 border-r-white bg-[#6ea76e] ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"}  md:hidden`
         }
       >
-        <h1 className="w-full text-3xl font-bold text-white m-8">
-          Book Share App
-        </h1>
+        <div className="flex items-center m-8">
+          <img
+            src="/images/bookShare.jpg"
+            className="w-[30px] h-[30px] rounded"
+          />
+          <h1 className="w-full text-xl font-bold text-white ms-2">
+            Book Share
+          </h1>
+        </div>
         <ul className="pt-4 uppercase p-4">
           <li className="p-4 border-b">Home</li>
           <li className="p-4 border-b">Dashboard</li>
