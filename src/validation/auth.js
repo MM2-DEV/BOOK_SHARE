5207bc09f8c3c2f2e5f09439d6610fc28d92a13a
AfrojaSultana44 <afrojasultanamoni311@gmail.com>
2024-03-23 01:04:17 +0600

    validation message changed

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -8,7 +8,7 @@ export const signInSchema = yup.object({
 export const signUpSchema = yup.object({
   name: yup.string().required("Username is required"),
   email: yup.string().email().required("Email is required"),
-  password: yup.string().required("Message is required"),
+  password: yup.string().required("Password is required"),
   session: yup.string().required("Session is required"),
   dob: yup.string().required("Date of Birth is required"),
 });
